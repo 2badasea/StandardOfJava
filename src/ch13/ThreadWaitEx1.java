61151d80d26c0e6523c426a9751ca002f78c8e03
badalee <gnjqtpf1@gmail.com>
2023-06-21 15:58:39 +0900

    2023. 06. 21 15:58 committed, chapter13 finished

---
+++ src/ch13/ThreadWaitEx1.java
@@ -0,0 +1,93 @@
+package ch13;
+
+import java.util.*;
+
+public class ThreadWaitEx1 {
+	public static void main(String[] args) throws Exception {
+		Table table = new Table(); // 여러 스레드가 공유하는 
+
+		new Thread(new Cook(table), "COOK1").start();
+		new Thread(new Customer(table, "donut"), "CUST1").start();
+		new Thread(new Customer(table, "burger"), "CUST2").start();
+
+		Thread.sleep(100);
+		System.exit(0);
+	}
+}
+
+class Customer implements Runnable {
+	private Table table;
+	private String food;
+
+	Customer(Table table, String food) {
+		this.table = table;
+		this.food = food;
+	}
+
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+			}
+			String name = Thread.currentThread().getName();
+
+			if (eatFood())
+				System.out.println(name + " ate a " + food);
+			else
+				System.out.println(name + " failed to eat. :(");
+		} // while
+	}
+
+	boolean eatFood() {
+		return table.remove(food);
+	}
+}
+
+class Cook implements Runnable {
+	private Table table;
+
+	Cook(Table table) {
+		this.table = table;
+	}
+
+	public void run() {
+		while (true) {
+
+			int idx = (int) (Math.random() * table.dishNum());
+			table.add(table.dishNames[idx]);
+
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+			}
+		} // while
+	}
+}
+
+class Table {
+	String[] dishNames = { "donut", "donut", "burger" };  // donut이 더 자주 나옴
+	final int MAX_FOOD = 6;
+
+	private ArrayList<String> dishes = new ArrayList<>();
+
+	public void add(String dish) {
+		if (dishes.size() >= MAX_FOOD)
+			return;
+		dishes.add(dish);
+		System.out.println("Dishes:" + dishes.toString());
+	}
+
+	public boolean remove(String dishName) {
+		for (int i = 0; i < dishes.size(); i++)
+			if (dishName.equals(dishes.get(i))) {
+				dishes.remove(i);
+				return true;  // 손님이 정상적으로 먹었다면 true를 리턴.
+			}
+		return false;
+	}
+
+	public int dishNum() {
+		return dishNames.length;
+	}
+}
